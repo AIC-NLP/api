789b3f6f33f2de65569d72faaa19e8ea9954d6bf
Rowan Tarek <rowantahseen@gmail.com>
2021-02-01 14:44:40 +0200

    added supported languages

--- translate.py
+++ translate.py
@@ -2,6 +2,10 @@ class Translator():
     def __init__(self):
         self.data={"zh":"I am chinese",
                     "ar":"I am egyptian"}
+        self.langs = ["zh -> ar" , "ar -> zh", "ar -> fr" , "fr -> ar"]
+
+    def supported_languages(self):
+        return self.langs
 
     def translate(self, src, tgt, text):
         return self.data[tgt]
